044849a76ae5cc30bc98da94e06da6fb3d4550f1
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2024-07-24 23:47:16 +0900

    Adapterパターン

---
+++ DesignPattern_0718/AdapterPattern.java
@@ -0,0 +1,23 @@
+class CostManeger{
+	public int compute(int a,int b){
+		return a * b;
+	}
+}
+
+class MoneyManager extends CostManeger implements Goods{
+	private int value;
+	private int amount;
+
+	public MoneyManager(int newValue,int newAmount){
+		value = newValue;
+		amount = newAmount;
+	}	
+
+	public int getPrice(){
+		int result = compute(value,amount);
+		reutrn result * 1.1;
+	}
+	
+	print(getPrice());
+
+}
